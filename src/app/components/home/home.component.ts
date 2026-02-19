c6dea780dabf9edf0272e1dd027c4c6cdbbb376a
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-02-19 08:54:09 -0300

    melhoria visual e arquitetural da pagina inicial(home)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TechIconsComponent } from "../tech-icons/tech-icons.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  imports: [TechIconsComponent]
 })
 export class HomeComponent {
 
